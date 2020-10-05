8fed7e3f9021ab88a4fc65aeb4503067257d1750
jpcutshall <jpcutshall@gmail.com>
2020-10-05 12:13:19 -0400

    basic js layout

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 console.log("all is connected")
+
+
+const App = {
+	gameOver: false,
+	createNewGrid: (size) => {
+		for (let i = 0; i < size; i++) {
+		 for (let j = 0; j < size; j++) {
+		  $('<div>').addClass("tile").appendTo(".game-box")
+		 }
+		}
+	},
+
+}
+
+const UI = {
+	creat2ndDiv: () => {},
+	gameResults: () => {},
+}
+
+
+$(() => {
+	App.createNewGrid(20)
+})
