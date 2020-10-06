6af4d44f7d4cd410892611de301eedc28ea02afd
jpcutshall <jpcutshall@gmail.com>
2020-10-06 13:12:05 -0400

    restructured for random mines

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 console.log("all is connected")
 
 
-const App = {
-	gameOver: false,
-	createNewGrid: (size) => {
-		for (let i = 0; i < size; i++) {
-		 for (let j = 0; j < size; j++) {
-		  $('<div>').addClass("tile").appendTo(".game-box")
-		 }
-		}
-	},
+//###################################################
+// 1st creating class for the individual squares on the grid
+//
+// class GameBox {
+// 	constructor () {
+// 		this.mine = true;
+// 		this.revealed = true;
+// 		this.x = 0;
+// 		this.y = 0;
+//
+// 	}
+// }
 
-}
+
 
 const UI = {
 	creat2ndDiv: () => {},
@@ -20,5 +23,31 @@ const UI = {
 
 
 $(() => {
-	App.createNewGrid(20)
+
+	let squares = []
+
+	const App = {
+		gameOver: false,
+		squares: [],
+		bombAmount: 20,
+		createNewGrid: (size) => {
+			const bombArray = Array(App.bombAmount).fill('bomb')
+			const emptyArray = Array(size - App.bombAmount).fill('clear')
+			const gameArray = emptyArray.concat(bombArray)
+			const shuffledArray = gameArray.sort(() => Math.random() -0.5)
+
+			for (let i = 0; i < size; i++) {
+				const tile = $('<div>').attr({
+					id: i,
+					class: shuffledArray[i]
+				}).appendTo(".game-box")
+				squares.push(tile)
+			 //$('<div>').addClass("tile").appendTo(".game-box")
+			}
+
+		},
+
+	}
+App.createNewGrid(50)
+
 })
