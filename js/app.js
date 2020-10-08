e460c772f1ae4734ab1c33c5f4c906781a06e9d7
jpcutshall <jpcutshall@gmail.com>
2020-10-08 12:05:22 -0400

    adding my event listeners - still have weird problem with diag boxe reading from edges and 2nd game box not adding right

--- js/app.js
+++ js/app.js
@@ -5,7 +5,35 @@ console.log("all is connected")
 // 1st creating class for the individual squares on the grid
 //
 
+const eventListeners = {
+	onePlayer: () => {					// will remove the second player div
+		const $secondDiv = $('.player2Box')
+		$secondDiv.remove()
+	},
+	twoPlayer: () => {
 
+	},
+	easy: () => {
+		App.difficulty = 33;
+	},
+	hard: () => {
+		App.difficulty = 66;
+	},
+	extreme: () => {
+		App.difficulty = 132;
+	},
+	click: (tile) => {
+		if (tile.hasClass('bomb')) {  // end game for player
+			alert(`you hit a mine!`)
+		}
+	},
+	start: () => {
+		App.createNewGrid()
+	},
+	stop: () => {
+
+	}
+}
 
 let grids1 = []
 let grids2 = []
@@ -37,7 +65,9 @@ const App = {
 				exy++
 			}
 
-
+			tile.on("click", (it) => {
+				eventListeners.click(tile)
+			})
 		}
 
 		for (let i = 0; i < grid.length; i++) {
@@ -144,7 +174,7 @@ const App = {
 				if (grid[i].is($leftEdge) && (!grid[i].is($topEdge) || !grid[i].is($bottomEdge)) && $(`div[x=${gridX}][y=${right}]`).hasClass('bomb')) { // LEft Edge work
 					counter++
 				}
-				if (grid[i].is($leftEdge) && (!grid[i].is($topEdge) || !grid[i].is($bottomEdge)) && $(`div[x=${above}][y=${right}]`).hasClass('bomb')) {
+				if (grid[i].is($leftEdge) && (!grid[i].is($topEdge) || !grid[i].is($bottomEdge)) && $(`div[x=${above}][y=${right}]`).hasClass('bomb')) { // square up one right one
 					counter++
 				}
 				if (grid[i].is($leftEdge) && (!grid[i].is($topEdge) || !grid[i].is($bottomEdge)) && $(`div[x=${below}][y=${right}]`).hasClass('bomb')) {
@@ -156,7 +186,7 @@ const App = {
 				if (grid[i].is($leftEdge) && (!grid[i].is($topEdge) || !grid[i].is($bottomEdge)) && $(`div[x=${below}][y=${gridY}]`).hasClass('bomb')) {
 					counter++
 				}
-				if (!grid[i].is($leftEdge) && !grid[i].is($rightEdge) && !grid[i].is($topEdge) && !grid[i].is($bottomEdge) && $(`div[x=${below}][y=${gridY}]`).hasClass('bomb')) { // squares not on an edge
+				if (!grid[i].is($leftEdge) && !grid[i].is($rightEdge) && !grid[i].is($topEdge) && !grid[i].is($bottomEdge) && $(`div[x=${below}][y=${gridY}]`).hasClass('bomb')) { // squares not on an edge -- Works perfect in first box but not second and other checkers are broken!?
 					counter++
 				}
 				if (!grid[i].is($leftEdge) && !grid[i].is($rightEdge) && !grid[i].is($topEdge) && !grid[i].is($bottomEdge) && $(`div[x=${above}][y=${gridY}]`).hasClass('bomb')) {
@@ -165,13 +195,13 @@ const App = {
 				if (!grid[i].is($leftEdge) && !grid[i].is($rightEdge) && !grid[i].is($topEdge) && !grid[i].is($bottomEdge) && $(`div[x=${above}][y=${left}]`).hasClass('bomb')) {
 					counter++
 				}
-				if (!grid[i].is($leftEdge) && !grid[i].is($rightEdge) && !grid[i].is($topEdge) && !grid[i].is($bottomEdge) && $(`div[x=${above}][y=${right}]`).hasClass('bomb')) {
+				if (!grid[i].is($leftEdge) && !grid[i].is($rightEdge) && !grid[i].is($topEdge) && !grid[i].is($bottomEdge) && $(`div[x=${above}][y=${right}]`).hasClass('bomb')) { // square up one right one
 					counter++
 				}
 				if (!grid[i].is($leftEdge) && !grid[i].is($rightEdge) && !grid[i].is($topEdge) && !grid[i].is($bottomEdge) && $(`div[x=${below}][y=${left}]`).hasClass('bomb')) {
 					counter++
 				}
-				if (!grid[i].is($leftEdge) && !grid[i].is($rightEdge) && !grid[i].is($topEdge) && !grid[i].is($bottomEdge) && $(`div[x=${below}][y=${right}]`).hasClass('bomb')) { 
+				if (!grid[i].is($leftEdge) && !grid[i].is($rightEdge) && !grid[i].is($topEdge) && !grid[i].is($bottomEdge) && $(`div[x=${below}][y=${right}]`).hasClass('bomb')) {
 					counter++
 				}
 				if (!grid[i].is($leftEdge) && !grid[i].is($rightEdge) && !grid[i].is($topEdge) && !grid[i].is($bottomEdge) && $(`div[x=${gridX}][y=${left}]`).hasClass('bomb')) {
@@ -190,102 +220,21 @@ const App = {
 	},
 }
 
-const eventListeners = {
-	onePlayer: () => {					// will remove the second player div
-		const $secondDiv = $('.player2Box')
-		$secondDiv.remove()
-	},
-	twoPlayer: () => {
 
-	},
-	easy: () => {
-		App.difficulty = 33;
-	},
-	hard: () => {
-		App.difficulty = 66;
-	},
-	extreme: () => {
-		App.difficulty = 132;
-	},
-	// small: () => {
-	// 	App.rows = 8;
-	// 	App.cols = 8;
-	// },
-	// medium: () => {
-	// 	App.rows = 12;
-	// 	App.cols = 12;
-	// },
-	// large: () => {
-	// 	App.rows = 16;
-	// 	App.cols = 16;
-	// },
-	start: () => {
-		App.createNewGrid()
-	},
-	stop: () => {
-
-	}
-}
 
 $(() => {
 
 	const $playOne = $('.player1Box')
 	const $playTwo = $('.player2Box')
 	App.createNewGrid($playOne, grids1)
-	App.createNewGrid($playTwo, grids1)
+
+	$("#1player").on('click', () => {
+		eventListeners.onePlayer()
+	})
+	$("#2player").on('click', () => {
+		App.createNewGrid($playTwo, grids2)
+	})
 
 
 
 })
-// $(() => {
-//
-//
-//
-// 	const App = {
-// 		gameOver: false,
-// 		squares: [],
-// 		bombAmount: 10,
-// 		width: 10,
-// 		flags: 0,
-//
-// 		createNewGrid: () => {
-// 			const bombArray = Array(App.bombAmount).fill('bomb')  //fill array with bombs
-// 			const emptyArray = Array(App.width*App.width - App.bombAmount).fill('clear')
-// 			const gameArray = emptyArray.concat(bombArray)
-// 			const shuffledArray = gameArray.sort(() => Math.random() -0.5) // randomizes position of bobms in the shuffles array
-//
-// 			for (let i = 0; i < App.width*App.width; i++) {			// creates individual tile divs and appends them to squares array
-// 				const tile = $('<div>').attr({
-// 					id: i,
-// 					class: shuffledArray[i]
-// 				}).appendTo(".game-box")
-// 				App.squares.push(tile)
-//
-// 			}
-//
-//
-// 			for (let i = 0; i < App.squares.length; i++) { // adding number values to squares
-//       let total = 0
-//       const isLeftEdge = (i % App.width === 0)
-//       const isRightEdge = (i % App.width === App.width - 1)
-//
-//       if (App.squares[i].hasClass('clear')) {
-//         if (i > 0 && !isLeftEdge && App.squares[i - 1].hasClass('bomb')) total ++
-// 				if (i > App.width - 1 && !isRightEdge && App.squares[i +1 - App.width].hasClass('bomb')) total ++
-//         if (i > App.width && App.squares[i - App.width].hasClass('bomb')) total ++
-//         if (i > App.width + 1 && !isLeftEdge && App.squares[i -1 - App.width].hasClass('bomb')) total ++
-//         if (i < App.width * App.width - 2 && !isRightEdge && App.squares[i +1].hasClass('bomb')) total ++
-//         if (i < App.width * App.width - App.width && !isLeftEdge && App.squares[i -1 + App.width].hasClass('bomb')) total ++
-//         if (i < App.width * App.width - App.width - 2 && !isRightEdge && App.squares[i +1 + App.width].hasClass('bomb')) total ++
-//         if (i < App.width * App.width - App.width - 1 && App.squares[i + App.width].hasClass('bomb')) total ++
-//         App.squares[i].attr('data', total)
-//       }
-//     }
-//
-// 		},
-//
-//
-// 	}
-// App.createNewGrid(198)
-//
-// })
